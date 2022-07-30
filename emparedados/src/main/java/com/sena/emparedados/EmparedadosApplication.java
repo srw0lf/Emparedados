e2c4c8a24e782f30042c6b6dea3b4f2fdef5d800
Arnold Retavisca <arnoldretavisca68@gmail.com>
2022-07-30 12:28:23 -0500

    se ha creado el proyecto emparedados

---
+++ emparedados/src/main/java/com/sena/emparedados/EmparedadosApplication.java
@@ -0,0 +1,13 @@
+package com.sena.emparedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmparedadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmparedadosApplication.class, args);
+	}
+
+}
---
+++ emparedados/src/test/java/com/sena/emparedados/EmparedadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.emparedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmparedadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
